f17a752b70c7e62d3b9ed48c3c95d4048a08b04d
Michael Lapcevich <149459288+lapcevichme@users.noreply.github.com>
2025-06-02 21:46:49 +0700

    docs(presentation): Add KDoc to MainActivity and MyApplication

--- app/src/main/java/com/lapcevichme/olympiadfinder/MyApplication.kt
+++ app/src/main/java/com/lapcevichme/olympiadfinder/MyApplication.kt
@@ -3,11 +3,12 @@ package com.lapcevichme.olympiadfinder
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Основной класс приложения.
+ *
+ * Аннотация [@HiltAndroidApp] генерирует базовый класс приложения,
+ * который используется Hilt для внедрения зависимостей на уровне приложения.
+ * Этот класс должен быть указан в манифесте приложения (`android:name`).
+ */
 @HiltAndroidApp
-class MyApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-
-    }
-}
+class MyApplication : Application() {}
